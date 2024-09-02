3e543054b897c63ebd1539755e1348fe9576841b
dafu <wuchengyi2006@163.com>
2024-09-02 07:24:57 +0000

    Update install_tensorrt.sh to latest

--- docker/common/install_tensorrt.sh
+++ docker/common/install_tensorrt.sh
@@ -5,14 +5,14 @@ set -ex
 TRT_VER="10.3.0.26"
 # Align with the pre-installed cuDNN / cuBLAS / NCCL versions from
 # https://docs.nvidia.com/deeplearning/frameworks/pytorch-release-notes/rel-24-05.html#rel-24-05
-CUDA_VER="12.5"
+CUDA_VER="12.5" # 12.5.1
 # Keep the installation for cuDNN if users want to install PyTorch with source codes.
-# PyTorch 2.3.x can compile with cuDNN v9.
-CUDNN_VER="9.3.0.75-1"
+# PyTorch 2.x can compile with cuDNN v9.
+CUDNN_VER="9.2.1.18-1"
 NCCL_VER="2.22.3-1+cuda12.5"
 CUBLAS_VER="12.5.3.2-1"
 # Align with the pre-installed CUDA / NVCC / NVRTC versions from
-# https://docs.nvidia.com/cuda/archive/12.4.1/cuda-toolkit-release-notes/index.html
+# https://docs.nvidia.com/cuda/archive/12.5.1/cuda-toolkit-release-notes/index.html
 NVRTC_VER="12.5.82-1"
 
 for i in "$@"; do
